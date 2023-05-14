e1f8445ec285186b64bd944dfcee596ebce71a5f
Chinmay Nivsarkar <chinmay.nivsarkar@github.com>
2023-05-14 14:15:17 -0400

    .

---
+++ include/MovingAverage.h
@@ -0,0 +1,131 @@
+/*
+MIT License
+
+Copyright (c) 2018 Pavel Slama
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE.
+*/
+
+#ifndef MOVINGAVERAGE_H
+#define MOVINGAVERAGE_H
+
+#if defined(__MBED__)
+  #include "mbed.h"
+#endif
+
+template <class T, uint16_t N>
+class MovingAverage {
+ public:
+  MovingAverage();
+  virtual ~MovingAverage(void);
+
+  T add(T value);
+  T get();
+  int32_t get_sum();
+  void fill(T value);
+  void reset();
+  void set_samples(uint16_t samples);
+
+ private:
+  bool _first;
+  uint8_t _next;
+  uint8_t _shift;
+  int32_t _sum;
+  uint16_t _samples;
+
+  T _buffer[N];
+  T _result;
+};
+
+template <class T, uint16_t N>
+MovingAverage<T, N>::MovingAverage():
+  _first(true),
+  _next(0),
+  _shift(0),
+  _sum(0),
+  _samples(N) {
+  _result = 0;
+
+  while (_samples >> _shift != 1) {
+    _shift++;
+  }
+}
+
+template <class T, uint16_t N>
+MovingAverage<T, N>::~MovingAverage(void) {
+}
+
+template <class T, uint16_t N>
+T MovingAverage<T, N>::get() {
+  return _result;
+}
+template <class T, uint16_t N>
+int32_t MovingAverage<T, N>::get_sum() {
+  return _sum;
+}
+
+template <class T, uint16_t N>
+T MovingAverage<T, N>::add(T value) {
+  // fill buffer when using first
+  if (_first) {
+    _first = false;
+    fill(value);
+
+  } else {
+    _sum = _sum - _buffer[_next] + value;
+    _buffer[_next] = value;
+    _next = (_next + 1) & (_samples - 1);
+  }
+
+  _result = (_sum + (_samples >> 1)) >> _shift;  // same as (_sum + (_samples / 2)) / _samples;
+
+  return _result;
+}
+
+template <class T, uint16_t N>
+void MovingAverage<T, N>::fill(T value) {
+  for (uint16_t i = 0; i < _samples; i++) {
+    _buffer[i] = value;
+  }
+
+  _sum = value * _samples;
+  _next = 0;
+}
+
+template <class T, uint16_t N>
+void MovingAverage<T, N>::reset() {
+  _first = true;
+}
+
+template <class T, uint16_t N>
+void MovingAverage<T, N>::set_samples(uint16_t samples) {
+  if (samples <= N) {
+    _samples = samples;
+
+    reset();
+
+    _shift = 0;
+
+    while (_samples >> _shift != 1) {
+      _shift++;
+    }
+  }
+}
+
+#endif
--- src/main.cpp
+++ src/main.cpp
@@ -1,205 +1,166 @@
 #include <mbed.h>
-#include <math.h>
-#include <vector>
-#include <cmath>
-#include "stm32f4xx_hal.h"
-#include "stm32f429i_discovery_lcd.h"
- 
-extern "C" void wait_ms(int ms) {
-  for (int i = 0; i < ms; i++) {
-    for (int j = 0; j < 1000; j++) {
-      __asm("nop");
-    }
-  }
+#include "MovingAverage.h"
+
+#define rad_limit 1.0
+#define reading_count 50
+
+SPI spi(PF_9, PF_8, PF_7,PC_1,use_gpio_ssel); // mosi, miso, sclk, cs
+
+//address of first register with gyro data
+#define OUT_X_L 0x28
+
+//register fields(bits): data_rate(2),Bandwidth(2),Power_down(1),Zen(1),Yen(1),Xen(1)
+#define CTRL_REG1 0x20
+
+//configuration: 200Hz ODR,50Hz cutoff, Power on, Z on, Y on, X on
+#define CTRL_REG1_CONFIG 0b01'10'1'1'1'1
+
+//register fields(bits): reserved(1), endian-ness(1),Full scale sel(2), reserved(1),self-test(2), SPI mode(1)
+#define CTRL_REG4 0x23
+//configuration: reserved,little endian,500 dps,reserved,disabled,4-wire mode
+#define CTRL_REG4_CONFIG 0b0'0'01'0'00'0
+
+//register fields(bits): reserved(1), endian-ness(1),Full scale sel(2), reserved(1),self-test(2), SPI mode(1)
+#define CTRL_REG5 0x24
+//configuration: reserved,little endian,500 dps,reserved,disabled,4-wire mode
+#define CTRL_REG5_CONFIG 0b00'00'00'10
+
+#define SPI_FLAG 1
+
+uint8_t write_buf[32]; 
+uint8_t read_buf[32];
+
+MovingAverage <uint8_t, 4> filter_x;
+MovingAverage <uint8_t, 4> filter_y;
+MovingAverage <uint8_t, 4> filter_z;
+
+
+float data_buffer[3][reading_count];
+float verify_buffer[3][reading_count];
+
+EventFlags flags;
+//The spi.transfer() function requires that the callback
+//provided to it takes an int parameter
+void spi_cb(int event){
+  flags.set(SPI_FLAG);
 }
 
-struct GyroData {
-  int16_t x, y, z;
-};
-
-const int threshold = 100;
-const int max_sequence_length = 300;
-volatile bool flag = false;
-SPI spi(PF_9, PF_8, PF_7);
-DigitalOut cs(PC_1); 
-InterruptIn enter_key(USER_BUTTON);
-
-void set_flag();
-void set_mode();
-void setup_gyro();
-void init_display();
-bool compare_sequences(const std::vector<GyroData> &seq1, const std::vector<GyroData> &seq2, double threshold);
-double compute_cross_correlation(const std::vector<double> &x, const std::vector<double> &y);
-GyroData read_gyro();
-
-// Define state variables
-enum State {
-  IDLE,
-  RECORDING,
-  WAITING,
-  UNLOCKING,
-  CHECKING,
-  LOCKED
-};
-
-State state = IDLE;
-
-void updateState() {
-  flag = true;
-}
-
-void setMode() {
-  cs = 0;
-  spi.write(0x20);
-  spi.write(0xCF);
-  cs = 1;
-}
-
-GyroData read_gyro() {
-  const int num_bytes = 6;
-  // Initialize buffer for reading data from SPI bus
-  std::array<uint8_t, num_bytes> buffer = {0};
-
-  // Send command to start data read
-  cs = 0;
-  spi.write(0xE8);
-  // Read data from SPI bus
-  for (int i = 0; i < num_bytes; i++) {
-    buffer[i] = spi.write(0x00);
-  }
-
-  cs = 1;
-  // Parse raw data from buffer
-  GyroData raw_data;
-  raw_data.x = (buffer[1] << 8) | buffer[0];
-  raw_data.y = (buffer[3] << 8) | buffer[2];
-  raw_data.z = (buffer[5] << 8) | buffer[4];
-
-  return raw_data;
-}
-
-void setup_gyro() {
-  cs = 1;
-  setMode();
-  spi.format(8, 3);
-  spi.frequency(100000);
-} 
-
-bool compare_sequences(const std::vector<GyroData>& seq1, const std::vector<GyroData>& seq2, double threshold) {
-  int n = std::max(seq1.size(), seq2.size());
-
-  std::vector<double> x1(n, 0), y1(n, 0), z1(n, 0), x2(n, 0), y2(n, 0), z2(n, 0);
-  std::transform(seq1.begin(), seq1.end(), x1.begin(), [](const auto& d) { return d.x; });
-  std::transform(seq1.begin(), seq1.end(), y1.begin(), [](const auto& d) { return d.y; });
-  std::transform(seq1.begin(), seq1.end(), z1.begin(), [](const auto& d) { return d.z; });
-  std::transform(seq2.begin(), seq2.end(), x2.begin(), [](const auto& d) { return d.x; });
-  std::transform(seq2.begin(), seq2.end(), y2.begin(), [](const auto& d) { return d.y; });
-  std::transform(seq2.begin(), seq2.end(), z2.begin(), [](const auto& d) { return d.z; });
-
-  return (compute_cross_correlation(x1, x2) > threshold) && (compute_cross_correlation(y1, y2) > threshold) && 
-         (compute_cross_correlation(z1, z2) > threshold);
-}
-
-double compute_cross_correlation(const std::vector<double>& x, const std::vector<double>& y) {
-  int n = x.size();
-  double max_corr = 0.0;
-  for (int shift = -n + 1; shift < n; shift++) {
-    double corr = 0.0;
-    for (int i = 0; i < n; i++) {
-      int j = i + shift;
-      if (j >= 0 && j < n) {
-        corr += x[i] * y[j];
-      }
-    }
-    max_corr = std::max(max_corr, corr / n);
-  }
-  return max_corr;
-}
-
-void init_display() {
-  HAL_Init();
-  BSP_LCD_Init();
-  BSP_LCD_LayerDefaultInit(0, LCD_FRAME_BUFFER);
-  BSP_LCD_SelectLayer(0);
-  BSP_LCD_DisplayOn();
-  BSP_LCD_Clear(LCD_COLOR_WHITE);
-}
 
 int main() {
-  setup_gyro();
-  init_display();
+    
+    int i = 0;
+    // Setup the spi for 8 bit data, high steady state clock,
+    // second edge capture, with a 1MHz clock rate
+    spi.format(8,3);
+    spi.frequency(1'000'000);
 
-  // Initialize arrays for recorded and attempted sequences
-  std::vector<GyroData> record(max_sequence_length);
-  std::vector<GyroData> attempt(max_sequence_length);
-  int index = 0;
-  int remaining_attempts = 5;
-  bool correct = false;
+    write_buf[0]=CTRL_REG1;
+    write_buf[1]=CTRL_REG1_CONFIG;
+    spi.transfer(write_buf,2,read_buf,2,spi_cb,SPI_EVENT_COMPLETE );
+    flags.wait_all(SPI_FLAG);
 
-  enter_key.fall(&updateState);
+    write_buf[0]=CTRL_REG4;
+    write_buf[1]=CTRL_REG4_CONFIG;
+    spi.transfer(write_buf,2,read_buf,2,spi_cb,SPI_EVENT_COMPLETE );
+    flags.wait_all(SPI_FLAG);
 
-  while (1) {
-    if (flag) {
-      BSP_LCD_Clear(LCD_COLOR_WHITE);
-      BSP_LCD_SetTextColor(LCD_COLOR_BLACK);
-      switch (state) {
-        case IDLE:
-          record.clear();
-          attempt.clear();
-          state = RECORDING;
-          break;
-        case RECORDING:
-          BSP_LCD_DisplayStringAt(0, LINE(5), (uint8_t *)"Recording...", CENTER_MODE);
-          state = WAITING;
-          break;
-        case WAITING:
-          BSP_LCD_DisplayStringAt(0, LINE(5), (uint8_t *)"Unlock?", CENTER_MODE);
-          state = UNLOCKING;
-          index = 0;
-          break;
-        case UNLOCKING:
-          state = CHECKING;
-          break;
-        case CHECKING:
-          printf("Running Sequence Comparison\n");
-          correct = compare_sequences(record, attempt, 100);
-          if (!correct) {
-            BSP_LCD_SetTextColor(LCD_COLOR_RED);
-            BSP_LCD_DisplayStringAt(0, LINE(5), (uint8_t *)"Incorrect :(", CENTER_MODE);
-            if (remaining_attempts == 0) {
-              state = LOCKED;
-              break;
-            } else {
-              state = UNLOCKING;
-              remaining_attempts--;
-              printf("Incorrect. %d attempts remaining...\n", remaining_attempts);
-              break;
-            }
-          } else {
-            BSP_LCD_SetTextColor(LCD_COLOR_GREEN);
-            BSP_LCD_DisplayStringAt(0, LINE(5), (uint8_t *)"Unlocked :)", CENTER_MODE);
-            state = IDLE;
-            break;
-          }
-        case LOCKED: 
-          BSP_LCD_SetTextColor(LCD_COLOR_RED);
-          BSP_LCD_DisplayStringAt(0, LINE(5), (uint8_t *)"LOCKED.", CENTER_MODE);
-          state = LOCKED;
-          break;
-        default:
-          state = IDLE;
-          break;
+    write_buf[0]=CTRL_REG5;
+    write_buf[1]=CTRL_REG5_CONFIG;
+    spi.transfer(write_buf,2,read_buf,2,spi_cb,SPI_EVENT_COMPLETE );
+    flags.wait_all(SPI_FLAG);  
+    printf("Configured, ready to start reading\n");
+    while (1) {
+    int16_t raw_gx,raw_gy,raw_gz;
+    float gx, gy, gz;
+      //prepare the write buffer to trigger a sequential read
+      write_buf[0]=OUT_X_L|0x80|0x40;
+      //start sequential sample reading
+      spi.transfer(write_buf,7,read_buf,7,spi_cb,SPI_EVENT_COMPLETE );
+      flags.wait_all(SPI_FLAG);
+      //read_buf after transfer: garbage byte, gx_low,gx_high,gy_low,gy_high,gz_low,gz_high
+      //Put the high and low bytes in the correct order lowB,HighB -> HighB,LowB
+      raw_gx=( ( (uint16_t)read_buf[2] ) <<8 ) | ( (uint16_t)read_buf[1] );
+      raw_gy=( ( (uint16_t)read_buf[4] ) <<8 ) | ( (uint16_t)read_buf[3] );
+      raw_gz=( ( (uint16_t)read_buf[6] ) <<8 ) | ( (uint16_t)read_buf[5] );
+
+      //printf("RAW|\tgx: %d \t gy: %d \t gz: %d\t",raw_gx,raw_gy,raw_gz);
+      
+      //gx=filter_x.add((int)((raw_gx)*(17.5f*0.017453292519943295769236907684886f / 1000.0f)));
+      //gy=filter_y.add((int)((raw_gy)*(17.5f*0.017453292519943295769236907684886f / 1000.0f)));
+      //gz=filter_z.add((int)((raw_gz)*(17.5f*0.017453292519943295769236907684886f / 1000.0f)));
+
+      gx=(int)((raw_gx)*(17.5f*0.017453292519943295769236907684886f / 1000.0f));
+      gy=(int)((raw_gy)*(17.5f*0.017453292519943295769236907684886f / 1000.0f));
+      gz=(int)((raw_gz)*(17.5f*0.017453292519943295769236907684886f / 1000.0f));
+
+      if(gx<rad_limit && gx>=-rad_limit)
+        gx = 0.0;
+      if(gy<rad_limit && gy>=-rad_limit)
+        gy = 0.0;
+      if(gz<rad_limit && gz>=-rad_limit)
+        gz = 0.0;
+      if(i<reading_count){
+        data_buffer[0][i] = gx;
+        data_buffer[1][i] = gy;
+        data_buffer[2][i] = gz;
+        i++;
       }
-      flag = false;
-    } else if (state == RECORDING || state == UNLOCKING) {
-      if (index < max_sequence_length) {
-        if (state == RECORDING) {
-          record[index] = read_gyro();
-        } else {
-          attempt[index] = read_gyro();
-        }
-        index++;
+      else{
+        break;
       }
+     thread_sleep_for(50);
     }
-  }
+    printf("Completed reading\n");
+    for(int i=0;i<reading_count;i++){
+      printf("%4.5f;%4.5f;%4.5f\n",data_buffer[0][i],data_buffer[1][i],data_buffer[2][i]);
+    }
+    thread_sleep_for(1000);
+    printf("Ready for new reading\n");
+    i = 0 ;
+    while (1) {
+    int16_t raw_gx,raw_gy,raw_gz;
+    float gx, gy, gz;
+    
+      //prepare the write buffer to trigger a sequential read
+      write_buf[0]=OUT_X_L|0x80|0x40;
+      //start sequential sample reading
+      spi.transfer(write_buf,7,read_buf,7,spi_cb,SPI_EVENT_COMPLETE );
+      flags.wait_all(SPI_FLAG);
+      //read_buf after transfer: garbage byte, gx_low,gx_high,gy_low,gy_high,gz_low,gz_high
+      //Put the high and low bytes in the correct order lowB,HighB -> HighB,LowB
+      raw_gx=( ( (uint16_t)read_buf[2] ) <<8 ) | ( (uint16_t)read_buf[1] );
+      raw_gy=( ( (uint16_t)read_buf[4] ) <<8 ) | ( (uint16_t)read_buf[3] );
+      raw_gz=( ( (uint16_t)read_buf[6] ) <<8 ) | ( (uint16_t)read_buf[5] );
+
+      //printf("RAW|\tgx: %d \t gy: %d \t gz: %d\t",raw_gx,raw_gy,raw_gz);
+      
+      //gx=filter_x.add((int)((raw_gx)*(17.5f*0.017453292519943295769236907684886f / 1000.0f)));
+      //gy=filter_y.add((int)((raw_gy)*(17.5f*0.017453292519943295769236907684886f / 1000.0f)));
+      //gz=filter_z.add((int)((raw_gz)*(17.5f*0.017453292519943295769236907684886f / 1000.0f)));
+
+      gx=(int)((raw_gx)*(17.5f*0.017453292519943295769236907684886f / 1000.0f));
+      gy=(int)((raw_gy)*(17.5f*0.017453292519943295769236907684886f / 1000.0f));
+      gz=(int)((raw_gz)*(17.5f*0.017453292519943295769236907684886f / 1000.0f));
+
+      if(gx<rad_limit && gx>=-rad_limit)
+        gx = 0.0;
+      if(gy<rad_limit && gy>=-rad_limit)
+        gy = 0.0;
+      if(gz<rad_limit && gz>=-rad_limit)
+        gz = 0.0;
+      if(i<reading_count){
+        verify_buffer[0][i] = gx;
+        verify_buffer[1][i] = gy;
+        verify_buffer[2][i] = gz;
+        i++;
+      }
+      else{
+        break;
+      }
+     thread_sleep_for(50);
+    }
+    printf("Completed Reading\n");
+    for(int i=0;i<reading_count;i++){
+      printf("%4.5f;%4.5f;%4.5f\n",verify_buffer[0][i],verify_buffer[1][i],verify_buffer[2][i]);
+    }
 }
