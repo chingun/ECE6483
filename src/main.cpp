38f8e3cf8b9a53b6aaa9d80340aec4e5f16d4385
chingun <chingun@umich.edu>
2023-05-14 06:48:00 -0400

    successfully displaying and retrieving gyroscope data and displaying on LCD. Need to fix bugs on gesture similarity checks

--- src/main.cpp
+++ src/main.cpp
@@ -1,107 +1,217 @@
 #include <mbed.h>
-
-SPI spi(PF_9, PF_8, PF_7,PC_1,use_gpio_ssel); // mosi, miso, sclk, cs
-InterruptIn int2(PA_2,PullDown);
-
-#define OUT_X_L 0x28
-//register fields(bits): data_rate(2),Bandwidth(2),Power_down(1),Zen(1),Yen(1),Xen(1)
-#define CTRL_REG1 0x20
-//configuration: 200Hz ODR,50Hz cutoff, Power on, Z on, Y on, X on
-#define CTRL_REG1_CONFIG 0b01'10'1'1'1'1
-//register fields(bits): reserved(1), endian-ness(1),Full scale sel(2), reserved(1),self-test(2), SPI mode(1)
-#define CTRL_REG4 0x23
-//configuration: reserved,little endian,500 dps,reserved,disabled,4-wire mode
-#define CTRL_REG4_CONFIG 0b0'0'01'0'00'0
-//register fields(bits): I1_Int1 (1), I1_Boot(1), H_Lactive(1), PP_OD(1), I2_DRDY(1), I2_WTM(1), I2_ORun(1), I2_Empty(1)
-#define CTRL_REG3 0x22
-//configuration: Int1 disabled, Boot status disabled, active high interrupts, push-pull, enable Int2 data ready, disable fifo interrupts                 
-#define CTRL_REG3_CONFIG 0b0'0'0'0'1'000
-
-#define SPI_FLAG 1
-#define DATA_READY_FLAG 2
-
-
-uint8_t write_buf[32];
-uint8_t read_buf[32];
-
-EventFlags flags;
-//The spi.transfer function requires that the callback
-//provided to it takes an int parameter
-void spi_cb(int event){
-  flags.set(SPI_FLAG);
-  
+#include <math.h>
+#include <vector>
+#include <cmath>
+#include "stm32f4xx_hal.h"
+#include "stm32f429i_discovery_lcd.h"
  
-
-};
-void data_cb(){
-  flags.set(DATA_READY_FLAG);
-  
- 
-
-};
- 
-int main() {
-  // Setup the spi for 8 bit data, high steady state clock,
-  // second edge capture, with a 1MHz clock rate
-  spi.format(8,3);
-  spi.frequency(1'000'000);
-
-  write_buf[0]=CTRL_REG1;
-  write_buf[1]=CTRL_REG1_CONFIG;
-  spi.transfer(write_buf,2,read_buf,2,spi_cb,SPI_EVENT_COMPLETE );
-  flags.wait_all(SPI_FLAG);
-
-  write_buf[0]=CTRL_REG4;
-  write_buf[1]=CTRL_REG4_CONFIG;
-  spi.transfer(write_buf,2,read_buf,2,spi_cb,SPI_EVENT_COMPLETE );
-  flags.wait_all(SPI_FLAG);
-
-
-  //configure the interrupt to call our function
-  //when the pin becomes high
-  int2.rise(&data_cb);
-
-
-  write_buf[0]=CTRL_REG3;
-  write_buf[1]=CTRL_REG3_CONFIG;
-  spi.transfer(write_buf,2,read_buf,2,spi_cb,SPI_EVENT_COMPLETE );
-  flags.wait_all(SPI_FLAG);
-
-  // The gyroscope sensor keeps its configuration between power cycles.
-  // This means that the gyroscope will already have it's data-ready interrupt
-  // configured when we turn the board on the second time. This can lead to
-  // the pin level rising before we have configured our interrupt handler.
-  // To account for this, we manually check the signal and set the flag
-  // for the first sample.
-  if(!(flags.get()&DATA_READY_FLAG)&&(int2.read()==1)){
-    flags.set(DATA_READY_FLAG);
+extern "C" void wait_ms(int ms) {
+  for (int i = 0; i < ms; i++) {
+    for (int j = 0; j < 1000; j++) {
+      __asm("nop");
+    }
   }
+}
+
+struct GyroData {
+  int16_t x, y, z;
+};
+
+const int threshold = 100;
+const int max_sequence_length = 300;
+volatile bool flag = false;
+SPI spi(PF_9, PF_8, PF_7);
+DigitalOut cs(PC_1); 
+InterruptIn enter_key(USER_BUTTON);
+
+void set_flag();
+void set_mode();
+void setup_gyro();
+void init_display();
+bool compare_sequences(const std::vector<GyroData> &seq1, const std::vector<GyroData> &seq2, double threshold);
+double compute_cross_correlation(const std::vector<double> &x, const std::vector<double> &y);
+GyroData read_gyro();
+
+// Define state variables
+enum State {
+  IDLE,
+  RECORDING,
+  WAITING,
+  UNLOCKING,
+  CHECKING,
+  LOCKED
+};
+
+State state = IDLE;
+
+void updateState() {
+  flag = true;
+}
+
+void setMode() {
+  cs = 0;
+  spi.write(0x20);
+  spi.write(0xCF);
+  cs = 1;
+}
+
+GyroData read_gyro() {
+  const int num_bytes = 6;
+  // Initialize buffer for reading data from SPI bus
+  std::array<uint8_t, num_bytes> buffer = {0};
+
+  // Send command to start data read
+  cs = 0;
+  spi.write(0xE8);
+  // Read data from SPI bus
+  for (int i = 0; i < num_bytes; i++) {
+    buffer[i] = spi.write(0x00);
+  }
+
+  cs = 1;
+  // Parse raw data from buffer
+  GyroData raw_data;
+  raw_data.x = (buffer[1] << 8) | buffer[0];
+  raw_data.y = (buffer[3] << 8) | buffer[2];
+  raw_data.z = (buffer[5] << 8) | buffer[4];
+
+  return raw_data;
+}
+
+void setup_gyro() {
+  cs = 1;
+  setMode();
+  spi.format(8, 3);
+  spi.frequency(100000);
+} 
+
+bool compare_sequences(const std::vector<GyroData>& seq1, const std::vector<GyroData>& seq2, double threshold) {
+  int n = std::max(seq1.size(), seq2.size());
+
+  std::vector<double> x1(n, 0), y1(n, 0), z1(n, 0), x2(n, 0), y2(n, 0), z2(n, 0);
+  std::transform(seq1.begin(), seq1.end(), x1.begin(), [](const auto& d) { return d.x; });
+  std::transform(seq1.begin(), seq1.end(), y1.begin(), [](const auto& d) { return d.y; });
+  std::transform(seq1.begin(), seq1.end(), z1.begin(), [](const auto& d) { return d.z; });
+  std::transform(seq2.begin(), seq2.end(), x2.begin(), [](const auto& d) { return d.x; });
+  std::transform(seq2.begin(), seq2.end(), y2.begin(), [](const auto& d) { return d.y; });
+  std::transform(seq2.begin(), seq2.end(), z2.begin(), [](const auto& d) { return d.z; });
+
+  return (compute_cross_correlation(x1, x2) > threshold) && 
+         (compute_cross_correlation(y1, y2) > threshold) && 
+         (compute_cross_correlation(z1, z2) > threshold);
+}
+
+double compute_cross_correlation(const std::vector<double>& x, const std::vector<double>& y) {
+  int n = x.size();
+  double max_corr = 0.0;
+  for (int shift = -n + 1; shift < n; shift++) {
+    double corr = 0.0;
+    for (int i = 0; i < n; i++) {
+      int j = i + shift;
+      if (j >= 0 && j < n) {
+        corr += x[i] * y[j];
+      }
+    }
+    max_corr = std::max(max_corr, corr / n);
+  }
+  return max_corr;
+}
+
+void init_display() {
+  HAL_Init();
+  BSP_LCD_Init();
+  BSP_LCD_LayerDefaultInit(0, LCD_FRAME_BUFFER);
+  BSP_LCD_SelectLayer(0);
+  BSP_LCD_DisplayOn();
+  BSP_LCD_Clear(LCD_COLOR_WHITE);
+}
+
+int main() {
+  setup_gyro();
+  init_display();
+
+  // Initialize arrays for recorded and attempted sequences
+  std::vector<GyroData> record(max_sequence_length);
+  std::vector<GyroData> attempt(max_sequence_length);
+  int index = 0;
+  int remaining_attempts = 5;
+  bool correct = false;
+
+  enter_key.fall(&updateState);
+
   while (1) {
-    int16_t raw_gx,raw_gy,raw_gz;
-    float gx, gy, gz;
-
-    //wait until new sample is ready
-    flags.wait_all(DATA_READY_FLAG);
-    //prepare the write buffer to trigger a sequential read
-    write_buf[0]=OUT_X_L|0x80|0x40;
-
-    //start sequential sample reading
-    spi.transfer(write_buf,7,read_buf,8,spi_cb,SPI_EVENT_COMPLETE );
-    flags.wait_all(SPI_FLAG);
-
-    //read_buf after transfer: garbage byte, gx_low,gx_high,gy_low,gy_high,gz_low,gz_high
-    //Put the high and low bytes in the correct order lowB,Highb -> HighB,LowB
-    raw_gx=( ( (uint16_t)read_buf[2] ) <<8 ) | ( (uint16_t)read_buf[1] );
-    raw_gy=( ( (uint16_t)read_buf[4] ) <<8 ) | ( (uint16_t)read_buf[3] );
-    raw_gz=( ( (uint16_t)read_buf[6] ) <<8 ) | ( (uint16_t)read_buf[5] );
-
-    gx=((float)raw_gx)*(17.5f*0.017453292519943295769236907684886f / 1000.0f);
-    gy=((float)raw_gy)*(17.5f*0.017453292519943295769236907684886f / 1000.0f);
-    gz=((float)raw_gz)*(17.5f*0.017453292519943295769236907684886f / 1000.0f);
-    
-    printf("Actual|\tgx: %d \t gy: %d \t gz: %d\n",(int)gx,(int)gy,(int)gz);
-    thread_sleep_for(100);
-    
-
+    if (flag) {
+      switch (state) {
+        case IDLE:
+          record.clear();
+          attempt.clear();
+          printf("IDLE -> RECORDING\n");
+          BSP_LCD_Clear(LCD_COLOR_WHITE);
+          state = RECORDING;
+          break;
+        case RECORDING:
+          printf("RECORDING -> WAITING\n");
+          BSP_LCD_Clear(LCD_COLOR_WHITE);
+          BSP_LCD_SetTextColor(LCD_COLOR_BLACK);
+          BSP_LCD_DisplayStringAt(0, LINE(5), (uint8_t *)"Recording...", CENTER_MODE);
+          state = WAITING;
+          break;
+        case WAITING:
+          printf("WAITING -> UNLOCKING\n");
+          BSP_LCD_Clear(LCD_COLOR_WHITE);
+          BSP_LCD_SetTextColor(LCD_COLOR_BLACK);
+          BSP_LCD_DisplayStringAt(0, LINE(5), (uint8_t *)"Unlock?", CENTER_MODE);
+          state = UNLOCKING;
+          index = 0;
+          break;
+        case UNLOCKING:
+          printf("UNLOCKING -> CHECKING\n");
+          state = CHECKING;
+          break;
+        case CHECKING:
+          printf("Checking sequences\n");
+          correct = compare_sequences(record, attempt, 100);
+          if (!correct) {
+            BSP_LCD_Clear(LCD_COLOR_WHITE);
+            BSP_LCD_SetTextColor(LCD_COLOR_RED);
+            BSP_LCD_DisplayStringAt(0, LINE(5), (uint8_t *)"Incorrect :(", CENTER_MODE);
+            if (remaining_attempts == 0) {
+              state = LOCKED;
+              break;
+            } else {
+              state = UNLOCKING;
+              remaining_attempts--;
+              printf("Incorrect. %d attempts remaining...\n", remaining_attempts);
+              break;
+            }
+          } else {
+            printf("Correct! Checking -> IDLE\n");
+            BSP_LCD_Clear(LCD_COLOR_WHITE);
+            BSP_LCD_SetTextColor(LCD_COLOR_GREEN);
+            BSP_LCD_DisplayStringAt(0, LINE(5), (uint8_t *)"Unlocked :)", CENTER_MODE);
+            state = IDLE;
+            break;
+          }
+        case LOCKED:
+          BSP_LCD_Clear(LCD_COLOR_WHITE);
+          BSP_LCD_SetTextColor(LCD_COLOR_RED);
+          BSP_LCD_DisplayStringAt(0, LINE(5), (uint8_t *)"LOCKED.", CENTER_MODE);
+          state = LOCKED;
+          break;
+        default:
+          state = IDLE;
+          break;
+      }
+      flag = false;
+    } else if (state == RECORDING || state == UNLOCKING) {
+      if (index < max_sequence_length) {
+        if (state == RECORDING) {
+          record[index] = read_gyro();
+        } else {
+          attempt[index] = read_gyro();
+        }
+        index++;
+      }
+    }
   }
 }
